d6ddb400a3d5634ac99c45125f5567e9a75523cb
Henry Lee <henryleeworld@gmail.com>
2021-04-23 07:45:10 +0800

    feat:更新最新資料

--- js/pie-chart.js
+++ js/pie-chart.js
@@ -196,6 +196,7 @@ var config = {
 };
 
 window.onload = function() {
+    Chart.plugins.register(ChartDataLabels);
     var ctx = document.getElementById('chart-area').getContext('2d');
     window.pieChart = new Chart(ctx, config);
 };
